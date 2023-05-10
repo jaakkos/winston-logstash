2a2a09d1dd1d66b4cf2d3f5d8bc0223c70d6ceed
Jaakko Suutarla <jaakko@suutarla.com>
2023-05-10 14:20:57 +0300

    Ignore in build phase

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['@babel/preset-typescript', '@babel/preset-env'],
   ignore: ['src/winston-logstash.test.ts',
-    'src/winston-logstash-latest.test.ts'],
+    'src/winston-logstash-latest.test.ts',
+    'src/connection.test.ts', 'src/manager.test.ts'],
   targets: 'node 6',
 };
