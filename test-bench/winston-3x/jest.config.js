b7b88426d317346e875cd1d4f730868370889271
Jaakko Suutarla <jaakko@suutarla.com>
2023-05-28 21:06:59 +0200

    Fix js file formatting

--- test-bench/winston-3x/jest.config.js
+++ test-bench/winston-3x/jest.config.js
@@ -31,7 +31,7 @@ module.exports = {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -154,12 +154,12 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/test/*_test.js"
+    '**/test/*_test.js',
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
-    "/node_modules/"
+    '/node_modules/',
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
